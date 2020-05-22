031585f7c10350411482adf17693b48f2b7441af
loiminh <loiho11082015@gmail.com>
2020-05-22 21:48:40 +0700

    add customer service

--- src/main/java/com/codegym/laptopmanager/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/laptopmanager/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.codegym.laptopmanager.service.impl;
 
 import com.codegym.laptopmanager.model.Customer;
+import com.codegym.laptopmanager.repository.CustomerRepository;
 import com.codegym.laptopmanager.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,23 +12,26 @@ import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements ICustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Override
     public Page<Customer> findAll(Pageable pageable) {
-        return null;
+        return customerRepository.findAll(pageable);
     }
 
     @Override
     public Optional<Customer> findById(Long id) {
-        return Optional.empty();
+        return customerRepository.findById(id);
     }
 
     @Override
     public void save(Customer customer) {
-
+        customerRepository.save(customer);
     }
 
     @Override
     public void remote(Long id) {
-
+        customerRepository.deleteById(id);
     }
 }
