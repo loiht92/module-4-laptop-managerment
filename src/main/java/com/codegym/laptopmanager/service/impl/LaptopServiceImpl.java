9e5c12124bb864780ff389ff4247924042107415
loiminh <loiho11082015@gmail.com>
2020-05-22 21:44:12 +0700

    add laptop service

--- src/main/java/com/codegym/laptopmanager/service/impl/LaptopServiceImpl.java
+++ src/main/java/com/codegym/laptopmanager/service/impl/LaptopServiceImpl.java
@@ -1,7 +1,9 @@
 package com.codegym.laptopmanager.service.impl;
 
 import com.codegym.laptopmanager.model.Laptop;
+import com.codegym.laptopmanager.repository.LaptopRepository;
 import com.codegym.laptopmanager.service.ILaptopService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,23 +12,26 @@ import java.util.Optional;
 
 @Service
 public class LaptopServiceImpl implements ILaptopService {
+    @Autowired
+    private LaptopRepository laptopRepository;
+
     @Override
     public Page<Laptop> findAll(Pageable pageable) {
-        return null;
+        return laptopRepository.findAll(pageable);
     }
 
     @Override
     public Optional<Laptop> findById(Long id) {
-        return Optional.empty();
+        return laptopRepository.findById(id);
     }
 
     @Override
     public void save(Laptop laptop) {
-
+        laptopRepository.save(laptop);
     }
 
     @Override
     public void remote(Long id) {
-
+        laptopRepository.deleteById(id);
     }
 }
