439b12e46528c7417e161a3e300c40c745df0211
loiminh <loiho11082015@gmail.com>
2020-05-23 06:34:47 +0700

    add cart service

--- src/main/java/com/codegym/laptopmanager/service/impl/CartServiceImpl.java
+++ src/main/java/com/codegym/laptopmanager/service/impl/CartServiceImpl.java
@@ -1,7 +1,9 @@
 package com.codegym.laptopmanager.service.impl;
 
 import com.codegym.laptopmanager.model.Cart;
+import com.codegym.laptopmanager.repository.CartRepository;
 import com.codegym.laptopmanager.service.ICartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,23 +12,26 @@ import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements ICartService {
+    @Autowired
+    private CartRepository cartRepository;
+
     @Override
     public Page<Cart> findAll(Pageable pageable) {
-        return null;
+        return cartRepository.findAll(pageable);
     }
 
     @Override
     public Optional<Cart> findById(Long id) {
-        return Optional.empty();
+        return cartRepository.findById(id);
     }
 
     @Override
     public void save(Cart cart) {
-
+        cartRepository.save(cart);
     }
 
     @Override
     public void remote(Long id) {
-
+        cartRepository.deleteById(id);
     }
 }
