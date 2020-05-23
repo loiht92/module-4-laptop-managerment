2943c680f04348155b8fba923a11a2d0e8cb382f
loiminh <loiho11082015@gmail.com>
2020-05-23 22:22:40 +0700

    Add GetMaping create laptop

--- src/main/java/com/codegym/laptopmanager/controller/LaptopController.java
+++ src/main/java/com/codegym/laptopmanager/controller/LaptopController.java
@@ -11,12 +11,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -65,6 +64,87 @@ public class LaptopController {
         ModelAndView modelAndView = new ModelAndView("laptop/list");
         modelAndView.addObject("laptops", laptops);
         return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showFormCreateLaptop(){
+        ModelAndView modelAndView = new ModelAndView("laptop/create");
+        modelAndView.addObject("laptop", new Laptop());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer, RedirectAttributes redirect){
+        byte[] byte1 =  customer.getFirstName().getBytes(StandardCharsets.ISO_8859_1);
+        byte[] byte2 =  customer.getLastName().getBytes(StandardCharsets.ISO_8859_1);
+        String decodedSignature1 = new String(byte1, StandardCharsets.UTF_8);
+        String decodedSignature2 = new String(byte2, StandardCharsets.UTF_8);
+        customer.setFirstName(decodedSignature1);
+        customer.setLastName(decodedSignature2);
+
+        customerService.save(customer);
+        redirect.addFlashAttribute("message", "create customer successfully !");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditCustomer(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/edit");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/customer/error");
+        }
+    }
+
+    @PostMapping("/edit")
+    public String updateCustomer(@ModelAttribute("customer") Customer customer, RedirectAttributes redirect){
+        byte[] byte1 =  customer.getFirstName().getBytes(StandardCharsets.ISO_8859_1);
+        byte[] byte2 =  customer.getLastName().getBytes(StandardCharsets.ISO_8859_1);
+        String decodedSignature1 = new String(byte1, StandardCharsets.UTF_8);
+        String decodedSignature2 = new String(byte2, StandardCharsets.UTF_8);
+        customer.setFirstName(decodedSignature1);
+        customer.setLastName(decodedSignature2);
+
+        customerService.save(customer);
+        redirect.addFlashAttribute("message","edit customer successfully !");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteCustomer(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        //customer.getName()?
+        //customer.firstName?
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/customer/error");
+        }
+    }
+    //delete
+    @PostMapping("/delete")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer, RedirectAttributes redirect){
+        customerService.remove(customer.getId());
+        redirect.addFlashAttribute("message", "delete customer successfully !");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView viewCustomer(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/view");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        }
+        else {
+            return new ModelAndView("/customer/error");
+        }
     }
 
 
