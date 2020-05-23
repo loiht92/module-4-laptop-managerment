d2290ca8f0e2e11c3d1bddd2e0f8944d1520b631
loiminh <loiho11082015@gmail.com>
2020-05-23 22:44:33 +0700

    add list Cart

--- src/main/java/com/codegym/laptopmanager/controller/CartController.java
+++ src/main/java/com/codegym/laptopmanager/controller/CartController.java
@@ -1,7 +1,46 @@
 package com.codegym.laptopmanager.controller;
 
+import com.codegym.laptopmanager.model.Cart;
+import com.codegym.laptopmanager.model.Laptop;
+import com.codegym.laptopmanager.service.ICartItemsService;
+import com.codegym.laptopmanager.service.ICartService;
+import com.codegym.laptopmanager.service.ILaptopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("cart")
 public class CartController {
+    @Autowired
+    private ILaptopService laptopService;
+
+    @Autowired
+    private ICartService cartService;
+
+    @Autowired
+    private ICartItemsService cartItemsService;
+
+    @ModelAttribute("cart")
+    public Cart shopCart(){
+        Cart cart =  new Cart();
+        cartService.save(cart);
+        return cart;
+    }
+
+    @GetMapping("/shop")
+    public ModelAndView showHomePage(Pageable pageable){
+        ModelAndView modelAndView = new ModelAndView("cart/list");
+        Page<Laptop> laptops = laptopService.findAll(pageable);
+        modelAndView.addObject("laptops",laptops);
+        return modelAndView;
+    }
+
+
+
 }
